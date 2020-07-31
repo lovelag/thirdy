0f71e5b632904a1207f101ada07a458ee44c8d00
lovelag <jbernlove@gmail.com>
2020-07-31 10:27:30 +0800

    Add files via upload

---
+++ se/src/routes.js
@@ -0,0 +1,14 @@
+
+
+const SongsController =  require('./controllers/SongsController')
+const AuthenticationController =  require('./controllers/AuthenticationController')
+const AuthenticationControllerPolicy = require('./policies/AuthenticationControllerPolicy')
+module.exports = (app) => {
+    app.post('/register',AuthenticationControllerPolicy.register, AuthenticationController.register) 
+    
+    app.post('/login',AuthenticationController.login) 
+    
+    app.get('/songs',SongsController.index) 
+
+    app.post('/songs',SongsController.post) 
+}
